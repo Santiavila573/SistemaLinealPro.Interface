38008fd6b084b99d74080ee46865c136c6be4822
santiago@gmail.com <gpc6257@gmail.com>
2024-02-07 21:59:14 -0800

    Add project files.

---
+++ SistemaLinealPro.Interface/Form1.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace SistemaLinealPro.Interface
+{
+    using System;
+    using System.Windows.Forms;
+
+    public partial class Form1 : Form
+    {
+        private TextBox textBoxX;
+        private TextBox textBoxM;
+        private TextBox textBoxB;
+        private TextBox textBoxY;
+        private Button buttonCalculate;
+
+        public void Form()
+        {
+            this.textBoxX = new TextBox();
+            this.textBoxM = new TextBox();
+            this.textBoxB = new TextBox();
+            this.textBoxY = new TextBox();
+            this.buttonCalculate = new Button();
+
+            // Aquí puedes configurar las propiedades de los TextBox y el Button
+            // como su ubicación, tamaño, etc.
+
+            this.buttonCalculate.Click += new EventHandler(this.buttonCalculate_Click);
+
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.textBoxM);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.buttonCalculate);
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double x = double.Parse(this.textBoxX.Text);
+                double m = double.Parse(this.textBoxM.Text);
+                double b = double.Parse(this.textBoxB.Text);
+
+                double y = m * x + b;
+
+                this.textBoxY.Text = y.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Por favor, asegúrate de que 'x', 'm' y 'b' son números.");
+            }
+        }
+    }
+}
---
+++ SistemaLinealPro.Interface/Form1.cs
@@ -0,0 +1,26 @@
+namespace SistemaLinealPro.Interface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            ClientSize = new Size(888, 564);
+            Name = "Form1";
+            ResumeLayout(false);
+        }
+    }
+}
